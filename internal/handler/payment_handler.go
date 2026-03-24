72f19b996cade7422686a86cc7792c5c03d07c94
EmberGale <timofeyfo@gmail.com>
2026-03-24 16:53:04 +0300

    Handler init

--- internal/handler/payment_handler.go
+++ internal/handler/payment_handler.go
@@ -1,1 +1,35 @@
 package handler
+
+import (
+	"GoRestSQL/internal/model"
+	"GoRestSQL/internal/service"
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type PaymentHandler struct {
+	service service.PaymentService
+}
+
+func NewPaymentHandler(service service.PaymentService) *PaymentHandler {
+	return &PaymentHandler{service: service}
+}
+
+func (pH *PaymentHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var payment model.Payment
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, err := pH.service.CreatePayment(&payment)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Payment " + strconv.Itoa(id)))
+}
