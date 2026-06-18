359f94ff4b28162530bc9e1a8f97c452eef650cc
EmberGale <timofeyfo@gmail.com>
2026-06-18 18:15:59 +0300

    Add get all payments

--- internal/repository/payment_postgresql.go
+++ internal/repository/payment_postgresql.go
@@ -128,5 +128,10 @@ func (p *PostgreSQLPaymentRepository) CreateWithOutbox(ctx context.Context, paym
 }
 
 func (p *PostgreSQLPaymentRepository) GetAllPayments(ctx context.Context) ([]model.Payment, error) {
-
+	query := "SELECT * FROM payments"
+	var payments []model.Payment
+	if err := p.DB.Select(&payments, query); err != nil {
+		return nil, err
+	}
+	return payments, nil
 }
